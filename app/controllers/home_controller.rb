3cfe5f997bebb90d9cdcffc29ae48eaf27c30ce5
Felipe Wielewski <felipewielewski@ateliware.local>
2021-03-27 19:14:02 -0300

    next_bus page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
+  include Rails.application.routes.url_helpers
+
   def index
+
+  end
+
+  def help
+    
+  end
+
+  def info
+  end
+
+  def next_bus
+  end
+
+  def repair
   end
 end
