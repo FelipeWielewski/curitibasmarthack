f11b2d2ae2861a22cb92e5d192b9f71458358746
Felipe Wielewski <felipewielewski@ateliware.local>
2021-03-28 08:02:14 -0300

    dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,6 +17,10 @@ class HomeController < ApplicationController
 
   def index;end
 
+  def dashboard
+    render :dashboard, layout: 'dashboard'
+  end
+
   def help;end
 
   def info
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/', to: 'home#index', as: :index
+  get '/dashboard', to: 'home#dashboard', as: :dashbord
   get '/help', to: 'home#help'
   get '/info', to: 'home#info'
   get '/next_bus', to: 'home#next_bus'
