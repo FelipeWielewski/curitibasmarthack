991d1cbac6741ab3add1df5e4b870626a271533b
Felipe Wielewski <felipewielewski@ateliware.local>
2021-03-27 21:23:00 -0300

    adding mock items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
   include Rails.application.routes.url_helpers
 
+  
+  NOME_RUA = 'Imaculada Conceição, 1174 - Prado Velho'
+  ONIBUS = [
+    { name: '050 - INTERBAIRROS V', number: '050', delay: 1 },
+    { name: '165 - UNIVERSIDADES', number: '165', delay: 5 },
+    { name: '175 - BOM RETIRO / PUC', number: '175', delay: 15 },
+    { name: '471 - V. SÃO PAULO', number: '471', delay: 23 },
+    { name: '472 - UBERABA', number: '472', delay: 35 },
+    { name: '475 - CANAL BELÉM', number: '475', delay: 42 },
+    { name: '614 - FAZENDINHA / PUC', number: '614', delay: 26 }
+  ]
+  
+  before_action :load_data
+
   def index;end
 
   def help;end
@@ -22,5 +36,12 @@ class HomeController < ApplicationController
 
   def sucesso
     @text = params[:text]
+  end
+
+  def load_data
+    @spot_name = NOME_RUA
+    @list_bus = ONIBUS
+
+    @bus = ONIBUS.detect { |b| b[:number] == params[:number] } if params[:number].present?
   end
 end
